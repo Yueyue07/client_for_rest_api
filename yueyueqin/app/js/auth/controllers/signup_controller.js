98eb361b5588677dc83e0d98291ba5d9a1346408
Yueyue07 <xww.qyy@gmail.com>
2016-06-02 16:59:08 -0700

    add semi colon

--- yueyueqin/app/js/auth/controllers/signup_controller.js
+++ yueyueqin/app/js/auth/controllers/signup_controller.js
@@ -1,7 +1,7 @@
 module.exports = function(app) {
   app.controller('SignupController',['$scope', '$location', 'userAuth', function($scope, $location, userAuth){
     $scope.signup = true;
-    console.log($scope.input)
+    console.log($scope.input);
     $scope.submit = function(input) {
       console.log(input);
       userAuth.createUser(input, function(err, res){
